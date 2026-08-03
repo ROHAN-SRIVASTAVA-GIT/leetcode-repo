da30053c122aae31b04d22bc02aa40053b1d915e
Rohan Srivastava <rohansrivastava3125@gmail.com>
2026-08-03 07:47:44 +0530

    0026: scaffold removeDuplicates method signature

---
+++ 0026-remove-duplicates-from-sorted-array/Solution.java
@@ -0,0 +1,12 @@
+// Yeh poora file ek "class" hai
+public class Solution {
+
+    // "public" = bahar se call ho sakta hai
+    // "int" = yeh method unique elements ki GINTI return karega
+    // "removeDuplicates" = method ka naam
+    // "int[] nums" = sorted array jisme duplicates hain
+    public int removeDuplicates(int[] nums) {
+        // TODO: core logic agle commit mein aayegi
+        return 0;
+    }
+}
