69dffdcf4cc73484af2a5c144e70d6685ef9657f
Rohan Srivastava <rohansrivastava3125@gmail.com>
2026-09-12 21:06:24 +0530

    0028: scaffold strStr method signature

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/Solution.java
@@ -0,0 +1,12 @@
+// Yeh poora file ek "class" hai
+public class Solution {
+
+    // "public" = bahar se call ho sakta hai
+    // "int" = yeh method starting index (ya -1 agar nahi mila) return karega
+    // "strStr" = method ka naam (LeetCode ka diya hua naam)
+    // "String haystack" = bada string, "String needle" = jo dhundhna hai
+    public int strStr(String haystack, String needle) {
+        // TODO: core logic agle commit mein aayegi
+        return -1;
+    }
+}
