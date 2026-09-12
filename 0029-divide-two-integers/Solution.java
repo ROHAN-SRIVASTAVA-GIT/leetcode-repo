e2690992ff21a2f7057f2a714f8073ef32b2677a
Rohan Srivastava <rohansrivastava3125@gmail.com>
2026-09-12 21:09:57 +0530

    0029: scaffold divide method signature

---
+++ 0029-divide-two-integers/Solution.java
@@ -0,0 +1,12 @@
+// Yeh poora file ek "class" hai
+public class Solution {
+
+    // "public" = bahar se call ho sakta hai
+    // "int" = yeh method division ka answer (integer) return karega
+    // "divide" = method ka naam
+    // "int dividend" = jise divide karna hai, "int divisor" = jisse divide karna hai
+    public int divide(int dividend, int divisor) {
+        // TODO: core logic agle commit mein aayegi
+        return 0;
+    }
+}
