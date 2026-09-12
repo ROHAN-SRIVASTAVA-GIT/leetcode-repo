9f658a89e06ec2285be4e0fad27b45d46aabb3bf
Rohan Srivastava <rohansrivastava3125@gmail.com>
2026-09-12 21:02:59 +0530

    0027: scaffold removeElement method signature

---
+++ 0027-remove-element/Solution.java
@@ -0,0 +1,12 @@
+// Yeh poora file ek "class" hai
+public class Solution {
+
+    // "public" = bahar se call ho sakta hai
+    // "int" = yeh method bache hue (valid) elements ki GINTI return karega
+    // "removeElement" = method ka naam
+    // "int[] nums" = array, "int val" = jo value hatani hai
+    public int removeElement(int[] nums, int val) {
+        // TODO: core logic agle commit mein aayegi
+        return 0;
+    }
+}
